15748efdd90c64d57671e41912592aee9f09c16e
rustam <odiloffr@gmail.com>
2024-07-01 19:20:00 +0500

    changed file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(123)
+# Ikki sonni kiritamiz
+son1 = float(input("Birinchi sonni kiriting: "))
+son2 = float(input("Ikkinchi sonni kiriting: "))
+
+# Sonlarni qo‘shamiz
+yigindi = son1 + son2
+
+# Natijani chiqaramiz
+print("Yig‘indi:", yigindi)
